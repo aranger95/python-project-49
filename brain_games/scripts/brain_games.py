ac222bac226fad730ee7ac3e6900e00c2b19d949
Евгений Огиенко <zo542352@yandex.ru>
2024-07-24 20:36:25 +0300

    добавил скрипт для игры "Прогрессия"

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 from cli import welcome_user
-from scripts.brain_even import game
+from scripts.brain_even import even
 from scripts.brain_calc import calc
+from scripts.brain_gcd import gcd
+from scripts.brain_progression import progression
 
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
-    # game()
-    calc()
+    # even()
+    # calc()
+    # gcd()
+    progression()
 
 
 if __name__ == '__main__':
